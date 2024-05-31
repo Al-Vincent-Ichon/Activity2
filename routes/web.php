d692dae27b3418b2f3616642ed457b79e02360b9
Al-Vincent-Ichon <164612997+Al-Vincent-Ichon@users.noreply.github.com>
2024-05-31 20:23:13 +0800

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserControllerController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/', [ProductController::class, 'index']);
+Route::get('/', [UserController::class, 'index']);
